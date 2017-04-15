81488bfd5b52b6b316304c363c63d091cc27ccfa
Sean McRoskey <_@_>
2017-04-14 20:19:48 -0700

    Initial Commit

---
+++ Twitter Client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twitter Client
+//
+//  Created by Sean McRoskey on 4/14/17.
+//  Copyright © 2017 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
